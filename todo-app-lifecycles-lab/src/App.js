da961f63203144a538d14202080044ce520f115a
tbriany <brianytaveras@pursuit.org>
2020-01-03 00:26:53 -0500

    toastify notifications working

--- todo-app-lifecycles-lab/src/App.js
+++ todo-app-lifecycles-lab/src/App.js
@@ -3,7 +3,10 @@ import './App.css';
 import Form from './Components/Form'
 import TodosList from './Components/TodosList'
 import TodoItem from './Components/TodoItem'
+import {toast} from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
+toast.configure()
 
 class App extends React.Component {
   constructor(){
@@ -21,6 +24,7 @@ class App extends React.Component {
     this.setState({
       todoList: [...todoList,todoItem]
     })
+    this.newTodoNotify()
   }
 
   handleInput = (event) => {
@@ -43,8 +47,30 @@ class App extends React.Component {
    this.setState({
      todoList: newArr
    })
-
+   
+   this.deletedTodoNotify(todo)
   }
+
+   newTodoNotify = () => {
+    const {todoItem} = this.state
+     toast('New todo created: ' + todoItem, 
+    { autoClose: 10000, 
+      className: 'green-background',
+      bodyClassName: 'green-background',
+      progressClassName: 'green-background'
+    })
+  };
+
+  deletedTodoNotify = (deletedTodo) => {
+    toast('Todo Deleted: ' + deletedTodo, 
+   { autoClose: 10000, 
+     className: 'red-background',
+     bodyClassName: 'red-background',
+     progressClassName: 'red-background'
+   })
+  }
+
+   
 
   render(){
   const {handleSubmit, handleInput, handleDelete} = this
